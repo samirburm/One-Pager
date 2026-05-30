8a46b861e7b4a9cb84dccc80e8751b3c6ea0722e
samirburm <samir.burm@gmail.com>
2026-05-30 11:52:44 +0200

    js in eigenes file gemacht, navigation responsive gemacht, prefered-color-scheme eingefügt, untere bilder bei imperssionen gleich hoch gemacht

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// Light/Dark Mode Button animation
+
+const themebutton = document.getElementById("themebutton");
+
+themebutton.addEventListener("click", () => {
+    document.body.classList.toggle("lightmode");
+
+    if (document.body.classList.contains("lightmode")) {
+        themebutton.textContent = "Dark Mode";
+    } else {
+        themebutton.textContent = "Light Mode";
+    }
+});
+
+
+// Join Button animation
+
+const joinbutton = document.getElementById("joinbutton");
+
+const welcomemessage = document.getElementById("welcomemessage");
+
+joinbutton.addEventListener("click", () => {
+    welcomemessage.textContent = "Welcome to HYBRID.";
+});
